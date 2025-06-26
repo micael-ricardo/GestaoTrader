83b64676db04c78c2d291c92e93b8146af5a3fc9
Micael Ricardo <micael.ricardo@outlook.com>
2025-06-26 11:11:29 -0300

    Ajustes responsividade mobile

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,5 +1,6 @@
-<div class="d-flex" style="min-height: 100vh;">
-    <nav class="bg-dark text-white p-3 d-none d-lg-block" style="width: 250px;">
+<div class="d-flex">
+    {{-- Sidebar Desktop --}}
+    <nav class="bg-dark text-white p-3 shadow-lg position-fixed top-0 start-0 vh-100 d-none d-lg-block" style="width: 250px; z-index: 1030;">
         <div class="text-center mb-4">
             <h5 class="fw-bold">TraderApp</h5>
         </div>
@@ -11,28 +12,32 @@
             </li>
         </ul>
     </nav>
-    <div class="d-lg-none">
-        <button class="btn btn-dark m-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileSidebar">
+
+    {{-- Sidebar Mobile (Offcanvas) --}}
+    <div class="d-lg-none position-fixed top-0 start-0 z-1031 p-2">
+        <button class="btn btn-dark" data-bs-toggle="offcanvas" data-bs-target="#mobileSidebar">
             <i class="bi bi-list"></i>
         </button>
+    </div>
 
-        <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="mobileSidebar">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title">TraderApp</h5>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
-            </div>
-            <div class="offcanvas-body">
-                <ul class="nav flex-column gap-2">
-                    <li class="nav-item">
-                        <a href="{{ route('trader') }}" class="btn btn-outline-light w-100 text-start">
-                            <i class="bi bi-graph-up-arrow me-2"></i> Nova Sessão
-                        </a>
-                    </li>
-                </ul>
-            </div>
+    <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="mobileSidebar">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title">TraderApp</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="nav flex-column gap-2">
+                <li class="nav-item">
+                    <a href="{{ route('trader') }}" class="btn btn-outline-light w-100 text-start">
+                        <i class="bi bi-graph-up-arrow me-2"></i> Nova Sessão
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
-    <main class="flex-grow-1 p-4 bg-black text-white">
+
+    {{-- Conteúdo principal --}}
+    <main class="flex-grow-1 p-4 bg-black text-white" style="margin-left: 250px; width: 100%;">
         <div class="card bg-dark text-white p-4 shadow">
             <h4 class="text-center mb-4">Dashboard de Trade</h4>
 
@@ -55,6 +60,7 @@
         </div>
     </main>
 </div>
+
 
 <script>
 document.addEventListener('livewire:load', function () {
